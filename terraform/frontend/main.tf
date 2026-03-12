164b271ded78f2c0a0043bdbd4d604103c043952
NOAH LEE <iamnoah@myself.com>
2026-03-12 17:17:06 -0500

    use new module version

--- terraform/frontend/main.tf
+++ terraform/frontend/main.tf
@@ -1,5 +1,5 @@
 module "website" {
-  source = "git::https://github.com/enpicie/tf-module-s3-cloudfront-website.git?ref=v1.0.0"
+  source = "git::https://github.com/enpicie/tf-module-s3-cloudfront-website.git?ref=v1.1.0"
 
   website_name        = var.app_name
   domain_name         = var.domain_name
