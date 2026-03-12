8d48ae682e544be9e59203f470363dbfb139d6da
NOAH LEE <iamnoah@myself.com>
2026-03-12 14:11:44 -0500

    add acm

--- terraform/frontend/main.tf
+++ terraform/frontend/main.tf
@@ -1,8 +1,10 @@
 module "website" {
   source = "git::https://github.com/enpicie/tf-module-s3-cloudfront-website.git?ref=v1.0.0"
 
-  website_name = var.app_name
-  source_files = "./dist" # Vite React standard output directory
+  website_name        = var.app_name
+  source_files        = "./dist" # Vite React standard output directory
+  domain_name         = var.domain_name
+  acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
 
   common_tags = {
     Project     = var.app_name
